a061187720e1e2881758ce0d36f78ad801c2c639
gt <gt>
2017-04-12 13:47:37 +0300

    Created Project and first classes

---
+++ Harkkatyo/Classit/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Classit.Models.BL_ViewModels;
+
+namespace Classit {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow()
+		{
+			InitializeComponent();
+			try {
+				WorkDayControls test = new WorkDayControls();
+				test.LoadFromMysql();
+				dgWorkDayItems.DataContext = test.WorkDayItems;
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+				throw;
+			}
+		}
+	}
+}
---
+++ Harkkatyo/Classit/Models/BL_ViewModels/WorkDayControls.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classit.Models;
+using MySql.Data.MySqlClient;
+
+namespace Classit.Models.BL_ViewModels {
+	class WorkDayControls {
+		public ObservableCollection<WorkDayItem> WorkDayItems { get; set; }
+		public void LoadFromMysql()
+		{
+			try {
+				ObservableCollection<WorkDayItem> workdayitems = new ObservableCollection<WorkDayItem>();
+				string connStr = ""; // TODO fill this!!!
+				string sql = "SELECT EmployeeID, WorkStartTime, WorkEndTime FROM WorkDays";
+				using (MySqlConnection conn = new MySqlConnection(connStr)) {
+					conn.Open();
+					using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+					using (MySqlDataReader reader = cmd.ExecuteReader()) {
+						while (reader.Read()) {
+							WorkDayItem s = new WorkDayItem();
+							s.EmployeeId = reader.GetInt32(0);
+							s.StartTime = reader.GetDateTime(1);
+							s.EndTime = reader.GetDateTime(2);
+							workdayitems.Add(s);
+						}
+						WorkDayItems = workdayitems;
+					}
+				}
+			}
+			catch {
+				throw;
+			}
+		}
+	}
+}
